a6c7aa8e05cf6f4809ee8cbde483fc9daaba326c
Malik Muhammad Arslan <malikarslan6162aus@gmail.com>
2024-09-04 20:50:11 +1000

    New Coins Prefebs Added

--- Assets/4- Scripts/Pickups/Coin.cs
+++ Assets/4- Scripts/Pickups/Coin.cs
@@ -8,12 +8,22 @@ public class Coin : MonoBehaviour
     [SerializeField] AudioClip coinSoundEffect;
 
 
+ //   [Header("Coin Move Animation")]
+  //  public bool moveCoin = false;
+  //  [SerializeField] float moveSpeed = 5.0f;
+  //  [SerializeField] GameObject moveTargetPosition;
+
+    void Start()
+    {
+     //   moveCoin = false;
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Fowl"))
         {
-            if(AudioManager.GetInstance() != null)
+         //   moveCoin = true;
+            if (AudioManager.GetInstance() != null)
             {
                 AudioManager.GetInstance().PlaySingleShotAudio(coinSoundEffect, 1.0f);
             }
@@ -21,9 +31,30 @@ public class Coin : MonoBehaviour
             {
                 ScoreManager.GetInstance().IncreaseRewardScore(1);
                 ScoreManager.GetInstance().SaveCurrencyData();
-            }           
-               Destroy(gameObject);
+            }
+            Destroy(gameObject);
         } 
 
     }
+
+    /* 
+    void Update()
+    {
+        MoveCoin();
+    }
+    void MoveCoin()
+    {
+        if (moveCoin)
+        {
+            float step = moveSpeed * Time.deltaTime; // Calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, moveTargetPosition.transform.position, step);
+
+            // Check if the coin has reached the target position
+            if (Vector3.Distance(transform.position, moveTargetPosition.transform.position) < 0.001f)
+            {
+                moveCoin = false; // Stop moving the coin
+            }
+        }
+    }
+    */
 }
